16a74dd737d50898f8848015522cf6d8f8bf6219
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-13 09:43:26 -0600

    deleted break tags

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Data Design</title>
+	</head>
+	<body>
+		<h1>Users flock to view, up (or down vote), and share content </h1>
+		<p>
+			users of this site are most likely between the ages of 19 and 30
+
+		</p>
+	</body>
+</html>

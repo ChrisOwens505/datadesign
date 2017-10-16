d9e606781e7bacb40701fb68900c27599f11b922
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-15 19:15:44 -0600

    edited my paragraph

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Data Design</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
 		<h1>Riddit Data Design</h1>
@@ -18,8 +19,33 @@
 			loved by people who love to voice thier opinion
 		</p>
 <h2>User Story</h2>
+		<p>"I want to check my city's local reddit page"</p>
+		<h3>Use Case/Interaction Flow</h3>
 		<ul>
-			<li></li>
+			<li>I click log in and give my username and password</li>
+			<li>The site logs me in</li>
+			<li>I click on the "search" bar</li>
+			<li>The site allows me to type in what i am looking for</li>
+			<li>I type in "Albuquerque</li>
+			<li>The site shows me posts and accounts related to Albuquerque</li>
+			<li>i click on the account that says Albuquerque</li>
+			<li>the site displays the accounts posts for my enjoyment</li>
+		</ul>
+		<h2>Conceptual Model</h2>
+		<h3>Profile</h3>
+		<ul>
+			<li>profileId</li>
+			<li>profileEmail</li>
+			<li>profileHash</li>
+			<li>profilePhone</li>
+			<li>profileSalt</li>
+		</ul>
+		<h3>comment</h3>
+		<ul>
+			<li>commentId</li>
+			<li>commentProfileId</li>
+			<li>commentContent</li>
+			<li>commentDate</li>
 		</ul>
 	</body>
 </html>

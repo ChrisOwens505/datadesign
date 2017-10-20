49e0d2ca973bb2c0144302973a36b64a0142866d
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-20 10:36:38 -0600

    added commas to clean up data-design sql

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -10,46 +10,45 @@ DROP TABLE IF EXISTS profile;
 CREATE TABLE profile(
 	--
 	--
-	profileid BINARY(16) NOT NULL
-	profileactivationtoken CHAR (32)
-	profilehandle VARCHAR (32) NOT NULL
-
+	profileid BINARY(16) NOT NULL,
+	profileactivationtoken CHAR (32),
+	profilehandle VARCHAR (32) NOT NULL,
 	--
-	profileemail VARCHAR(128)
+	profileemail VARCHAR(128),
 	--
-	profilehash   CHAR(128) NOT NULL
-	profilephone VARCHAR(32)
-	profilesalt CHAR(64) NOT NULL
-	UNIQUE(profileemail)
-	UNIQUE(profilephone)
+	profilehash CHAR(128) NOT NULL,
+	profilephone VARCHAR(32),
+	profilesalt CHAR(64) NOT NULL,
+	UNIQUE(profileemail),
+	UNIQUE(profilephone),
 	--
 	PRIMARY Key(profileid)
 );
 --
 CREATE TABLE post(
-	postid BINARY(16) NOT NULL
-	postprofileid BINARY(16) NOT NULL
-	postcontent VARCHAR(140 NOT NULL
+	postid BINARY(16) NOT NULL,
+	postprofileid BINARY(16) NOT NULL,
+	postcontent VARCHAR(140 NOT NULL,
 		--
-	postdate DATETIME(6)	NOT NULL
+	postdate DATETIME(6)	NOT NULL,
 		--
-	INDEX(postprofileid)
+	INDEX(postprofileid),
 	--
-	FOREIGN KEY(postprofileid) REFERENCES profile(profileid)
+	FOREIGN KEY(postprofileid) REFERENCES profile(profileid),
 	--
 	PRIMARY KEY(postid)
 	);
 CREATE TABLE vote(
 	--
-	voteprofileid BINARY(16)NOT NULL
-	votepostid BINARY(16) NOT NULL
-	votdate DATETIME(6) NOT NULL
+	voteprofileid BINARY(16)NOT NULL,
+	votepostid BINARY(16) NOT NULL,
+	votdate DATETIME(6) NOT NULL,
 		--
-	INDEX(voteprofileid)
-	INDEX (votepostid)
+	INDEX(voteprofileid),
+	INDEX (votepostid),
 	--
-	FOREIGN KEY(voteprofileid) REFERENCES profile(profileid)
-	FOREIGN KEY(votepostid) REFERENCES post(postid)
+	FOREIGN KEY(voteprofileid) REFERENCES profile(profileid),
+	FOREIGN KEY(votepostid) REFERENCES post(postid),
 	--
 	PRIMARY KEY(voteprofileid, votepostid)
 );

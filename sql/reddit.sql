9d3dce9b7381cf0b6487bed987c09a06566423a6
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-22 19:15:14 -0600

    added tables to reddit.sql

--- sql/reddit.sql
+++ sql/reddit.sql
@@ -1,2 +1,52 @@
-mysql>CREATE TABLE pet (name VARCHAR(20), owener VARCHAR(20)
- -> species VARCHAR (20)
+-- DROP TABLE IF EXISTS vote;
+-- DROP TABLE IF EXISTS post;
+-- DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile(
+	profileid BINARY(16) NOT NULL,
+	profileactivationtoken CHAR (32),
+	profilehandle VARCHAR (32) NOT NULL,
+	profileemail VARCHAR(128),
+	profilehash CHAR(128) NOT NULL,
+	profilephone VARCHAR(32),
+	profilesalt CHAR(64) NOT NULL,
+	UNIQUE(profileemail),
+	UNIQUE(profilephone),
+	PRIMARY Key(profileid)
+);
+--
+CREATE TABLE post(
+	postid BINARY(16) NOT NULL,
+	postprofileid BINARY(16) NOT NULL,
+	postcontent VARCHAR(140) NOT NULL,
+	postdate DATETIME(6)	NOT NULL,
+	INDEX(postprofileid),
+	FOREIGN KEY(postprofileid) REFERENCES profile(profileid),
+	PRIMARY KEY(postid)
+);
+
+CREATE TABLE vote(
+	voteprofileid BINARY(16)NOT NULL,
+	votepostid BINARY(16) NOT NULL,
+	votdate DATETIME(6) NOT NULL,
+	INDEX(voteprofileid),
+	INDEX (votepostid),
+	FOREIGN KEY(voteprofileid) REFERENCES profile(profileid),
+	FOREIGN KEY(votepostid) REFERENCES post(postid),
+	PRIMARY KEY(voteprofileid, votepostid)
+);
+
+CREATE TABLE comments(
+	commentsid BINARY(16) NOT NULL,
+	commentsprofileid BINARY(16) NOT NULL,
+	commentspostid BINARY(16),
+	commentsCommentsid BINARY(16),
+	commentscontent VARCHAR(2000)NOT NULL,
+	commentsdate DATETIME(6) NOT NULL,
+	INDEX(commentsprofileid),
+	INDEX(commentspostid),
+	FOREIGN KEY(commentsprofileid) REFERENCES profile(profileid),
+	FOREIGN KEY(commentspostid) REFERENCES post(postid),
+	FOREIGN KEY (commentsCommentsid) REFERENCES comments(commentsid),
+	PRIMARY KEY(commentsid)
+);

501b6f98fd2c2565602555c83f4a84f8f3672cce
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-24 10:42:19 -0600

    cleaned up data design sql statements

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -3,70 +3,70 @@
 --
 --
 --
-DROP TABLE IF EXISTS vote;
-DROP TABLE IF EXISTS post;
-DROP TABLE IF EXISTS profile;
+-- DROP TABLE IF EXISTS vote;
+-- DROP TABLE IF EXISTS post*/;
+-- DROP TABLE IF EXISTS profile;
 --
 CREATE TABLE profile(
 --
 --
-	     profileid BINARY(16) NOT NULL,
-	     profileactivationtoken CHAR (32),
-	     profilehandle VARCHAR (32) NOT NULL,
+	     profileId BINARY(16) NOT NULL,
+	     profileActivationToken CHAR (32),
+	     profileHandle VARCHAR (32) NOT NULL,
 --
-	     profileemail VARCHAR(128),
+	     profileEmail VARCHAR(128),
 --
-	     profilehash CHAR(128) NOT NULL,
-	     profilephone VARCHAR(32),
-	     profilesalt CHAR(64) NOT NULL,
-	     UNIQUE(profileemail),
-	     UNIQUE(profilephone),--
-	     PRIMARY Key(profileid)
+	     profileHash CHAR(128) NOT NULL,
+	     profilePhone VARCHAR(32),
+	     profileSalt CHAR(64) NOT NULL,
+	     UNIQUE(profileEmail),
+	     UNIQUE(profilePhone),--
+	     PRIMARY Key(profileId)
 --
 );
 --
 CREATE TABLE post(
-	     postid BINARY(16) NOT NULL,
-	     postprofileid BINARY(16) NOT NULL,
-	     postcontent VARCHAR(140) NOT NULL,
---
-	     postdate DATETIME(6)	NOT NULL,
---
-	     INDEX(postprofileid),
---
-	     FOREIGN KEY(postprofileid) REFERENCES profile(profileid),
---
-	     PRIMARY KEY(postid)
-	);
+	postId BINARY(16) NOT NULL,
+	postProfileId BINARY(16) NOT NULL,
+	postContent VARCHAR(240) NOT NULL,
+	--
+	postDate DATETIME(6)	NOT NULL,
+	--
+	INDEX(postProfileId),
+	--
+	FOREIGN KEY(postProfileId) REFERENCES profile(profileId),
+	--
+	PRIMARY KEY(postId)
+);
 CREATE TABLE vote(
---
-	     voteprofileid BINARY(16)NOT NULL,
-	     votepostid BINARY(16) NOT NULL,
-	     votdate DATETIME(6) NOT NULL,
---
-	     INDEX(voteprofileid),
-	     INDEX (votepostid),
---
-	     FOREIGN KEY(voteprofileid) REFERENCES profile(profileid),
-	     FOREIGN KEY(votepostid) REFERENCES post(postid),
---
-	     PRIMARY KEY(voteprofileid, votepostid)
+	--
+	voteProfileId BINARY(16)NOT NULL,
+	votePostId BINARY(16) NOT NULL,
+	votDate DATETIME(6) NOT NULL,
+	--
+	INDEX(voteProfileId),
+	INDEX (votePostId),
+	--
+	FOREIGN KEY(voteProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(votePostId) REFERENCES post(postId),
+	--
+	PRIMARY KEY(voteProfileId, votePostId)
 );
 CREATE TABLE comments(
---
-	     commentsid BINARY(16) NOT NULL,
-	     commentsprofileid BINARY(16) NOT NULL,
-	     commentspostid BINARY(16),
-	     commentsCommentsid BINARY(16),
-	     commentscontent VARCHAR(2000)NOT NULL,
-	     commentsdate DATETIME(6) NOT NULL,
---
-	     INDEX(commentsprofileid),
-	     INDEX(commentspostid),
---
-		  FOREIGN KEY(commentsprofileid) REFERENCES profile(profileid),
-	     FOREIGN KEY(commentspostid) REFERENCES post(postid),
-	     FOREIGN KEY (commentsCommentsid) REFERENCES comments(commentsid),
---
-	     PRIMARY KEY(commentsid)
+	--
+	commentsId BINARY(16) NOT NULL,
+	commentsProfileId BINARY(16) NOT NULL,
+	commentsPostId BINARY(16),
+	commentsCommentsId BINARY(16),
+	commentsContent VARCHAR(2000)NOT NULL,
+	commentsDate DATETIME(6) NOT NULL,
+	--
+	INDEX(commentsProfileId),
+	INDEX(commentsPostId),
+	--
+	FOREIGN KEY(commentsProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(commentsPostId) REFERENCES post(postId),
+	FOREIGN KEY (commentsCommentsId) REFERENCES comments(commentsId),
+	--
+	PRIMARY KEY(commentsId)
 );
